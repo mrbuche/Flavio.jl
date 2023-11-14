733914849ae808f2c9c1c8d2b13ea0de7295be3b
mrbuche <dev@mrbuche.com>
2023-11-14 16:36:03 -0700

    works

--- src/Flavio.jl
+++ src/Flavio.jl
@@ -3,13 +3,7 @@ module Flavio
 const FLAVIOSO = string(dirname(@__FILE__), "/../deps/flavioso/")
 const FLAVIOSO_LIB = string(FLAVIOSO, "target/release/libflavioso")
 
-function inverse_langevin(x::Float64)::Float64
-    ccall((:inverse_langevin, FLAVIOSO_LIB), Float64, (Float64,), x)
-end
-
-function langevin(x::Float64)::Float64
-    ccall((:langevin, FLAVIOSO_LIB), Float64, (Float64,), x)
-end
+include("math.jl")
 
 export inverse_langevin, langevin
 

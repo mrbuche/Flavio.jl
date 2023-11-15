cbbe1e361020cea9e18573d81a21c37a59cf3faa
mrbuche <dev@mrbuche.com>
2023-11-15 11:13:46 -0700

    oh so thats FP...

--- src/constitutive.jl
+++ src/constitutive.jl
@@ -1,19 +1,17 @@
-function neo_hookean(κ::Float64, μ::Float64, F::Matrix{Float64})::Matrix{Float64}
+using StaticArrays
+
+struct NeoHookeanModel
+    κ::Float64
+    μ::Float64
+end
+
+function cauchy_stress(model::NeoHookeanModel, F::SMatrix{3,3,Float64})::SMatrix{3,3,Float64}
     output = ccall(
-        (:neo_hookean, FLAVIOSO_LIB),
-        Tuple{Float64,Float64,Float64,
-              Float64,Float64,Float64,
-              Float64,Float64,Float64},
-        (Float64, Float64, 
-        Float64, Float64, Float64, 
-        Float64, Float64, Float64, 
-        Float64, Float64, Float64),
-        κ, μ,
-        F[1, 1], F[1, 2], F[1, 3],
-        F[2, 1], F[2, 2], F[2, 3],
-        F[3, 1], F[3, 2], F[3, 3]
+        (:neo_hookean_cauchy_stress, FLAVIOSO_LIB), Ptr{Float64},
+        (Float64, Float64, Ptr{Float64}),
+        model.κ, model.μ, F
     )
-    return [output[1] output[2] output[3];
-            output[4] output[5] output[6];
-            output[7] output[8] output[9]]
+    return SMatrix{3,3,Float64}(
+        unsafe_wrap(Array{Float64}, output, 9, own=false)
+    )
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,18 @@
-using Flavio, Test
+using LinearAlgebra, Flavio, StaticArrays, Test
 
-@testset "Neo-Hookean" begin
-    κ = 13.0
-    μ = 13.0
-    F = [1.0 0.0 0.0;
-         0.0 1.0 0.0;
-         0.0 0.0 1.0]
-    σ = Flavio.neo_hookean(κ, μ, F)
-    for σ_i in σ
-        for σ_ij in σ_i
-            @test σ_ij == 0.0
-        end
-    end
+κ = 13.0
+μ = 3.0
+
+ϵ = 1e-6
+
+Identity = SMatrix{3, 3, Float64}(1, 0, 0, 0, 1, 0, 0, 0, 1)
+Zero = zeros(SMatrix{3, 3, Float64})
+SimpleShearSmall = SMatrix{3, 3, Float64}(1, 0, 0, ϵ, 1, 0, 0, 0, 1)
+VolumetricSmall = Identity * (1 + ϵ)^(1/3)
+
+@testset "Neo-Hookean model" begin
+    model = NeoHookeanModel(κ, μ)
+    @test cauchy_stress(model, Identity) == Zero
+    @test abs(cauchy_stress(model, SimpleShearSmall)[4]/ϵ/μ - 1) < ϵ
+    @test abs(tr(cauchy_stress(model, VolumetricSmall))/3/ϵ/κ - 1) < ϵ
 end

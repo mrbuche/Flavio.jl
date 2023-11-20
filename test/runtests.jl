955d3d2e42fa8b383589f8c7118d1b8a51b456ed
mrbuche <dev@mrbuche.com>
2023-11-20 11:21:10 -0700

    JET

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,12 @@
-using Aqua, Flavio, JuliaFormatter, LinearAlgebra, Test
+using Aqua, Flavio, JET, JuliaFormatter, LinearAlgebra, Test
 
 ϵ = 1e-6
 
 include("constitutive.jl")
 
 Aqua.test_all(Flavio)
+
+println("\nJET:\n", report_package("Flavio"; toplevel_logger = nothing))
 
 if !format("../", overwrite = false, verbose = true)
     error("File(s) not properly formatted.")
